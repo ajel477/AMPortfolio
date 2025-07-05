09ac581366ba0b016d7e77a982fe5f7f3167dc09
ajel477 <ajelmathew04@gmail.com>
2025-07-05 16:07:08 +0530

    smheropic

--- src/components/HeroSection/HeroPic.jsx
+++ src/components/HeroSection/HeroPic.jsx
@@ -9,25 +9,20 @@ const HeroPic = () => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0 }}
-      className="relative h-full flex items-center justify-center group"
+      className="relative flex items-center group justify-center w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] md:w-[350px] md:h-[350px] lg:w-[400px] lg:h-[400px] mx-auto"
     >
-      {/* Profile Image */}
-      <img
-        src="/pic.png"
-        alt="Ajel Mathew"
-        className="relative left-4 z-10 md:h-[550px] sm:h-[550px] object-cover sm:translate-x-16 sm:-translate-y-4"
-      />
+      {/* Profile Image inside hexagon */}
+      <div className="absolute w-full h-full overflow-hidden clip-hexagon sm:left-14 sm:-bottom-10 sm:h-[400px] md:-bottom-4 md:left-14 md:h-[400px]  lg:left-20 lg:-bottom-14 lg:h-[550px]">
+        <img
+          src="/pic.png"
+          alt="Ajel Mathew"
+          className="object-cover w-full h-full"
+        />
+      </div>
 
-      {/* Spinning Hexagon */}
-      <div className="absolute -z-10 flex justify-center items-center">
-        <div
-          className="
-            md:h-[400px] sm:h-[400px] w-auto text-cyan blur-md
-            animate-[spin_10s_linear_infinite] group-hover:animate-[spin_2s_linear_infinite]
-          "
-        >
-          <PiHexagonThin className="w-full h-full text-cyan" />
-        </div>
+      {/* Spinning Hexagon Border */}
+      <div className="absolute w-full h-full -z-10 animate-[spin_10s_linear_infinite] group-hover:animate-[spin_2s_linear_infinite] blur-md">
+        <PiHexagonThin className="w-full h-full text-cyan" />
       </div>
     </motion.div>
   );
